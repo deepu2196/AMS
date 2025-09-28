ee1b081f290022819dad27a1852496cf517f9640
Pradeep <pradeep2196@gmail.com>
2025-09-29 00:21:34 +0530

    get username in jwt decode

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -77,7 +77,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)) -> GetUserResponse:
             logger.log("Retrieving Payload.")
             payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
             logger.log(f"Payload retrieved: {payload}.")
-            username: str = payload.get("sub")
+            username: str = payload.get("username")
             if username is None:
                 raise credentials_exception
         except JWTError:
